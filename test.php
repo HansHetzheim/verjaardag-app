9e5a0c035b98aff20e5b3ae746c59dde73c3a5c9
Tom <tom.goyens@gmail.com>
2019-01-24 12:22:43 +0100

    php en sql test bestand update

--- test.php
+++ test.php
@@ -14,6 +14,21 @@
       <label for='email' >Your Email Address*:</label>
       <input type='text' name='email' id='email' maxlength="50" />
 
+      <label for='artist1' >Artist (1)*:</label>
+      <input type='text' name='artist1' id='artist1' maxlength="50" />
+      <label for='songName1' >Song Name*:</label>
+      <input type='text' name='songName1' id='songName1' maxlength="50" />
+
+      <label for='artist2' >Artist (2)*:</label>
+      <input type='text' name='artist2' id='artist2' maxlength="50" />
+      <label for='songName2' >Song Name (2)*:</label>
+      <input type='text' name='songName2' id='songName2' maxlength="50" />
+
+      <label for='artist3' >Artist (3)*:</label>
+      <input type='text' name='artist3' id='artist3' maxlength="50" />
+      <label for='songName' >Song Name (3)*:</label>
+      <input type='text' name='songName3' id='songName3' maxlength="50" />
+
       <input type='submit' name='Submit' value='Submit' />
 
       </fieldset>
@@ -26,17 +41,22 @@
 $servername = "localhost";
 $username = "tom";
 $password = "DaRuler07";
-$dbname = "test2";
-$tableName = "guests";
+$dbname = "dirkParty";
+$tableName = "guestPlaylist";
 $mailPattern = '/^[\w][\w.]+[\w]@[\w.]+\.[\w]{2,4}$/i';
 $doubleDot = "/\.{2,}/";
 $update = False;
 
 $name = $_POST['username'];
-echo $name . "<br>";
-
 $mail = $_POST['email'];
-echo $mail . "<br>";
+$artist1 = $_POST['artist1'];
+$songName1 = $_POST['songName1'];
+$artist2 = $_POST['artist2'];
+$songName2 = $_POST['songName2'];
+$artist3 = $_POST['artist3'];
+$songName3 = $_POST['songName3'];
+
+echo $artist1." ".$songName1." ".$artist2." ".$songName2." ".$artist3." ".$songName3."<br>";
 
 preg_match($mailPattern, $mail, $matches1, PREG_UNMATCHED_AS_NULL);
 print_r($matches1);
@@ -52,35 +72,41 @@ if ($conn->connect_error) {
    die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully<br>";
-//
+
 // // Create database-----------------------------------------------------------
-// // $sql = "CREATE DATABASE $dbname";
-// // if ($conn->query($sql) === TRUE) {
-// //     echo "Database created successfully<br>";
-// // } else {
-// //     echo "Error creating database: " . $conn->error;
-// // }
-//
+// $sql = "CREATE DATABASE $dbname";
+// if ($conn->query($sql) === TRUE) {
+//     echo "Database created successfully<br>";
+// } else {
+//     echo "Error creating database: " . $conn->error;
+// }
+
 //Connect to relevant database--------------------------------------------------
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 echo "Connected successfully to database " . $dbname . "<br>";
 //
 // //Create table---------------------------------------------------------------
-// // $sql = "CREATE TABLE $tableName(
-// //   id INTEGER(6) AUTO_INCREMENT PRIMARY KEY,
-// //   username VARCHAR(30) NOT NULL,
-// //   email VARCHAR(50) NOT NULL,
-// //   reg_date TIMESTAMP)";
-// // if ($conn->query($sql) === TRUE) {
-// //     echo "Table created successfully<br>";
-// // } else {
-// //     echo "Error creating table: " . $conn->error;
-// // }
-//
+// $sql = "CREATE TABLE $tableName(
+//   id INTEGER(6) AUTO_INCREMENT PRIMARY KEY,
+//   username VARCHAR(30) NOT NULL,
+//   email VARCHAR(50) NOT NULL UNIQUE,
+//   artist1 VARCHAR(30),
+//   songTitle1 VARCHAR(30),
+//   artist2 VARCHAR(30),
+//   songTitle2 VARCHAR(30),
+//   artist3 VARCHAR(30),
+//   songTitle3 VARCHAR(30),
+//   reg_date TIMESTAMP)";
+// if ($conn->query($sql) === TRUE) {
+//     echo "Table created successfully<br>";
+// } else {
+//     echo "Error creating table: " . $conn->error;
+// }
+
 // Add row----------------------------------------------------------------------
 // if ($matches1 != NULL && $matches2[0] === NULL){
-//   $sql = "INSERT INTO $tableName (username, email)
-//   VALUES ('$name', '$mail')";
+//   $sql = "INSERT INTO $tableName (username, email, artist1, songTitle1, artist2, songTitle2, artist3, songTitle3)
+//   VALUES ('$name', '$mail', '$artist1', '$songName1', '$artist2', '$songName2', '$artist3', '$songName3')";
 //
 //   if ($conn->query($sql) === TRUE) {
 //       $last_id = $conn->insert_id;
@@ -156,23 +182,54 @@ echo "Connected successfully to database " . $dbname . "<br>";
 // }
 
 //check of een ingevoerde email al in de database staat-------------------------
-$sql = "SELECT id FROM $tableName WHERE email = '$mail'";
-$result = $conn->query($sql);
-print_r($result->num_rows);
-echo "<br>";
+// $sql = "SELECT id FROM $tableName WHERE email = '$mail'";
+// $result = $conn->query($sql);
+// print_r($result->num_rows);
+// echo "<br>";
 
 //update record/row-------------------------------------------------------------
-if ($result->num_rows > 0) {
-    echo "Email exists.<br>";
-    $sql2 = "UPDATE $tableName SET username='AAAAAAA' WHERE email='$mail'";
-    if ($conn->query($sql2) === TRUE) {
-        echo "Record updated successfully";
-    } else {
-        echo "Error upadting record: " . $conn->error;
-    }
+
+if ($matches1 != NULL && $matches2[0] === NULL){
+  //check of een ingevoerde email al in de database staat
+  $sql = "SELECT id FROM $tableName WHERE email = '$mail'";
+  $result = $conn->query($sql);
+  print_r($result->num_rows);
+  echo "<br>";
+  //als het al bestaat, UPDATE, anders INSERT
+  if ($result->num_rows > 0) { //update
+      echo "Email exists.<br>";
+      $sql2 = "UPDATE $tableName
+      SET username='$name',
+      artist1='$artist1',
+      songTitle1='$songName1',
+      artist2='$artist2',
+      songTitle2='$songName2',
+      artist3='$artist3',
+      songTitle3='$songName3'
+      WHERE email='$mail'";
+
+      if ($conn->query($sql2) === TRUE) {
+          echo "Record updated successfully";
+      } else {
+          echo "Error upadting record: " . $conn->error;
+      }
+  } else { //insert
+      echo "Email not found.<br>";
+      $sql = "INSERT INTO $tableName (username, email, artist1, songTitle1, artist2, songTitle2, artist3, songTitle3)
+      VALUES ('$name', '$mail', '$artist1', '$songName1', '$artist2', '$songName2', '$artist3', '$songName3')";
+
+      if ($conn->query($sql) === TRUE) {
+          $last_id = $conn->insert_id;
+          echo "Row created successfully<br>Last inserted ID is: " . $last_id . "<br>";
+      } else {
+          echo "Error creating row: " . $conn->error;
+        }
+  }
 } else {
-    echo "Email not found.<br>";
+  echo "Invalid e-mail.<br>";
 }
+
+
 
 //Remove database-----------------------------------------------------------
 // $sql = "DROP DATABASE $dbname";
